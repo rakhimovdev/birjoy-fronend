fb58646e19f3df34a057900c405ec2c4b95805b0
Muhammadali <rahimov.dev1@gmail.com>
2026-06-10 15:42:59 +0500

    first commit2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,3 @@
 export const backendApiBaseUrl =
   process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, '') ||
-  (process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : '');
+  (process.env.NODE_ENV === 'development' ? 'https://birjoy-backend.onrender.com/api' : '');
